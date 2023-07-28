48cd120ae04a51a32a8b489598e0174ea6ae3bf8
Niferu <wargacki.patryq@gmail.com>
2023-07-28 20:34:22 +0200

    encoding function

--- contracts/DelegateCall.sol
+++ contracts/DelegateCall.sol
@@ -27,3 +27,19 @@ contract A {
         if (success) {}
     }
 }
+
+contract CallAnything {
+    address public s_someAddress;
+    uint256 public s_amount;
+
+    function transfer(address someAddress, uint256 amount) public {
+        s_someAddress = someAddress;
+        s_amount = amount;
+    }
+
+    function getSelectorOne() public pure returns (bytes4 selector) {
+        selector = bytes4(keccak256(bytes("transfer(address,uint256)")));
+
+        return selector;
+    }
+}
